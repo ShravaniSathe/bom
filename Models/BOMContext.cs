22881a93b3d78d90cbbc4efe6246f875054b593b
ShravaniSathe1 <122014949+ShravaniSathe1@users.noreply.github.com>
2024-09-15 14:21:01 +0530

    Add project files.

---
+++ Models/BOMContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace bom.Models
+{
+    public class BOMContext : DbContext
+    {
+        public BOMContext(DbContextOptions<BOMContext> options) : base(options) { }
+
+        public DbSet<ItemMasterSales> ItemMasterSales { get; set; }
+        public DbSet<ItemMasterRawMaterial> ItemMasterRawMaterial { get; set; }
+        public DbSet<SubAssemblies> SubAssemblies { get; set; }
+        public DbSet<BoughtOutItems> BoughtOutItems { get; set; }
+        public DbSet<BOMStructure> BOMStructures { get; set; }
+        public DbSet<ItemRawMaterialMapping> ItemRawMaterialMappings { get; set; }
+        public DbSet<RawMaterialBoughtOutMapping> RawMaterialBoughtOutMappings { get; set; }
+    }
+}
